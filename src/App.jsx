f80f0e9af7051c1792784a33d852b650482d9b96
Lazar Antonijevic <lazar.antonijevic@appollo41.com>
2023-11-26 23:48:27 +0100

    Basic router and pages created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import { About, Cocktail, Error, HomeLayout, Landing, Contact } from './pages';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout></HomeLayout>,
+    children: [
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: 'cocktail',
+        element: <Cocktail />,
+      },
+      {
+        path: 'newsletter',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
 const App = () => {
-  return <h2>shakeIt</h2>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
